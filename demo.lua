0068e67c990b295cb3188934eb659b7bfb290fc9
Đoàn Phạm <86369986+doanpbvn@users.noreply.github.com>
2025-06-12 05:16:14 +0700

    Create demo.lua

---
+++ demo.lua
@@ -0,0 +1,57 @@
+-- --------------------------------------
+-- Information of recording
+-- Time: 2025-06-12 05:12:34
+-- Resolution: 750, 1334
+-- Front most app: TikTokLite
+-- Orientation of front most app: Portrait
+-- --------------------------------------
+
+appActivate("com.ss.iphone.ugc.tiktok.lite");
+
+
+touchDown(3, 210.41, 852.62);
+usleep(98706.58);
+touchUp(3, 210.41, 852.62);
+usleep(184553.17);
+
+touchDown(6, 393.10, 823.10);
+usleep(82150.75);
+touchUp(6, 393.10, 823.10);
+usleep(184522.54);
+
+touchDown(4, 486.51, 821.07);
+usleep(99106.46);
+touchUp(4, 486.51, 821.07);
+usleep(1313841.88);
+
+keyDown(KEY_TYPE.HOME_BUTTON);
+usleep(89505.08);
+keyUp(KEY_TYPE.HOME_BUTTON);
+usleep(91743.00);
+
+keyDown(KEY_TYPE.HOME_BUTTON);
+usleep(14994.00);
+
+keyDown(KEY_TYPE.HOME_BUTTON);
+usleep(64104.08);
+keyUp(KEY_TYPE.HOME_BUTTON);
+usleep(661457.29);
+
+touchDown(5, 656.89, 792.55);
+usleep(15495.33);
+touchMove(5, 657.92, 768.12);
+usleep(16682.04);
+touchMove(5, 657.92, 730.45);
+usleep(16631.92);
+touchMove(5, 666.13, 686.66);
+usleep(16419.79);
+touchMove(5, 684.61, 631.68);
+usleep(16663.33);
+touchMove(5, 719.50, 568.56);
+usleep(16482.46);
+touchUp(5, 743.11, 516.64);
+usleep(867089.08);
+
+touchDown(1, 344.87, 1242.56);
+usleep(148791.46);
+touchUp(1, 344.87, 1242.56);
